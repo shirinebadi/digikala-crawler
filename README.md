7bd2a53ad8bb1288397949e5c57578ad89e81596
shirin.ebadi <shirin.ebadi@snapp.cab>
2022-01-23 19:33:14 +0330

    feat: crawling whole categories

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # digikala-crawler
-A crawler developed by Scray to crawl product informations (category, name, id, rates, image and price) and also comments of a desirable category.
+A crawler developed by Scrapy to crawl products information(category, name, id, rates, image and price) and also comments of whole categories.
 
 # Run the Crawler
 After downloading the project, navigate to digikalacrawler/spiders and run bellow command.
--- digikalacrawler/spiders/tripper.py
+++ digikalacrawler/spiders/tripper.py
@@ -1,4 +1,5 @@
-
+from curses import meta
+from markupsafe import string
 import scrapy, json
 
 ids = []
@@ -6,13 +7,19 @@ ids = []
 class Tripper(scrapy.Spider):
     name = 'Tripper'
 
-    start_urls = ['https://www.digikala.com/search/category-traveling-equipment']
+    start_urls = ['https://www.digikala.com/']
 
     def parse(self, response):
+        main_categories = response.css('.c-navi-new-list__sublist-option--item a.c-navi-new__big-display-title::attr(href)').getall()
+        for href in main_categories:
+            yield response.follow(href,self.parse_categories, meta = {'href': href} )
+
+    def parse_categories(self, response):
         last_page = int(response.css('a.c-pager__next::attr(data-page)').get())
 
         for i in range (1, last_page+1):
-            yield response.follow(f'https://www.digikala.com/search/category-traveling-equipment/pageno={i}', self.responsible)
+            href = response.meta.get('href')
+            yield response.follow(f'{href}/pageno={i}', self.responsible)
 
     def responsible(self, response):
         for i in range(1,37):
@@ -21,39 +28,26 @@ class Tripper(scrapy.Spider):
 
             comments_url = f'https://www.digikala.com/ajax/product/comments/{json_item["id"]}'
             
-            comments = response.follow(comments_url, self.comments, meta={'product-id': json_item["id"], 'product-name': json_item["name"], 'image': image})
+            comments = response.follow(comments_url, self.comments, meta={'product-id': json_item["id"], 'product-name': json_item["name"], 'image': image, 'price': json_item["price"]})
 
             yield comments
             
     def comments(self, response):
 
         comments = response.css('div.c-comments__item')
+        rate = response.css('.c-comments__side-rating-main::text').get()
+        total_no = response.css('.c-comments__side-rating-all::text').get().strip().split(" ")[-2]
 
         for comment in comments:
             yield {
                 'product-id': response.meta.get('product-id'),
                 'product-name': response.meta.get('product-name', None),
                 'image': response.meta.get('image', None),
+                'Rate': rate + ' out of 5',
+                'Number of Rates': total_no,
+                'Price': response.meta.get('price'),
                 'Date': comment.css('span.c-comments__detail::text').getall()[0],
                 'Owner': comment.css('span.c-comments__detail::text').getall()[-1],
                 'text': comment.css('div.c-comments__content::text').get()
             }
-
-class Rates(scrapy.Spider):
-    name = 'Rates'
-
-    start_urls = [f'https://www.digikala.com/ajax/product/comments/{id}' for id in ids]
-
-    def parse(self, response):
-        rate = response.css('.c-comments__side-rating-main::text').get()
-        total_no = response.css('.c-comments__side-rating-all::text').get().strip().split(" ")[-2]
-
-        yield {
-                'Rate': rate + ' out of 5',
-                'Number of Rates': total_no,
-            }
-            
-
-
-           
             
